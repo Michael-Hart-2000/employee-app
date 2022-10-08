2ac5bc7ec21492f66ff11a51f915641291626bfd
Your Name <you@example.com>
2022-10-09 00:17:17 +0100

    Lab 02 -Add menu function -Update main to control menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,40 @@ package me.michael
 
 import kotlin.math.round
 
-fun main(args: Array<String>) {
-    println("Pay Slip Printer")
-    println(getFullName())
-    println(getPayslip())
-    println("Monthly Salary: ${getMonthlySalary()}")
-    println("Monthly PRSI: ${getMonthlyPRSI()}")
-    println("Monthly PAYE: ${getMonthlyPAYE()}")
-    println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
-    println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
-    println("Monthly Net Pay: ${getNetMonthlyPay()}")
+fun main(args: Array<String>){
 
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+}
+
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readLine()!!.toInt()
 }
 
 fun getFullName() = when (gender){
