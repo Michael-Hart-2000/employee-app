7f61dee865ea92833c467906da8bd7bf57993f0e
Your Name <you@example.com>
2022-10-08 22:03:00 +0100

    Lab 02 -Add string templates in place of raw strings -Add function to format numbers rounded to two decimal places -Add tab insert to clean up spaces in payslip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
+package me.michael
+
+import kotlin.math.round
+
 fun main(args: Array<String>) {
     println("Pay Slip Printer")
     payslip()
 }
+
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
 
 val firstName = "Joe"
 val secondName = "Soap"
@@ -15,20 +21,21 @@ val cycleToWorkSchemeMonthlyDeduction = 54.33
 
 fun payslip() {
     println ("---------------------------------------------------------- ")
-    println ("|                 Monthly Payslip                        |")
+    println ("|                     Monthly Payslip                    |")
     println ("|--------------------------------------------------------|")
     println ("|                                                        |")
-    println ("|  Employee Name: " + firstName.uppercase() + " " +secondName.uppercase() + "("+gender.uppercase()+")"+ "      "+"Employee ID:"+employeeId + "      |")
+    println ("|  Employee Name: ${firstName.uppercase()} ${secondName.uppercase()} ${gender.uppercase()} \t\t Employee ID: $employeeId   |")
     println ("|                                                        |")
     println ("|--------------------------------------------------------|")
-    println ("|  PAYMENT DETAILS"+ "             "+ "DEDUCTION DETAILS         |")
+    println ("|  PAYMENT DETAILS \t\t\t DEDUCTION DETAILS           |")
     println ("|--------------------------------------------------------|")
-    println ("|  Salary: " + "%.2f".format (grossSalary/12) + "             " + "PAYE: " +"%.2f".format ((grossSalary/12*payePercentage/100))+ "             |")
-    println ("|  Bonus:  " + "%.2f".format (annualBonusAmountOf/12) + "              " + "PRSI: " +"%.2f".format (grossSalary/12*prsiPercentage/100)+ "              |")
-    println ("|                              " + "Cylce To Work: " + "%.2f".format ( cycleToWorkSchemeMonthlyDeduction)+ "      |")
+    println ("|  Salary: ${roundTwoDecimals(grossSalary/12)}            PAYE: ${roundTwoDecimals(grossSalary/12*payePercentage/100)}               |")
+    println ("|  Bonus:  ${roundTwoDecimals(annualBonusAmountOf/12)}            PRSI: ${roundTwoDecimals(grossSalary/12*prsiPercentage/100)}                |")
+    println ("|                            Cylce To Work: ${roundTwoDecimals(cycleToWorkSchemeMonthlyDeduction)}        |")
     println ("|--------------------------------------------------------|")
-    println ("|  Gross: " +  "%.2f".format(grossSalary/12 + annualBonusAmountOf/12) + "              " + "Total Deductions:" + "%.2f".format ((grossSalary/12*payePercentage/100 + grossSalary/12*prsiPercentage/100 + cycleToWorkSchemeMonthlyDeduction))+ "  |")
+    println ("|  Gross: ${roundTwoDecimals(grossSalary/12 + annualBonusAmountOf/12)}            Total Deductions:${roundTwoDecimals((grossSalary/12*payePercentage/100 + grossSalary/12*prsiPercentage/100 + cycleToWorkSchemeMonthlyDeduction))}    |")
     println ("|--------------------------------------------------------|")
-    println ("|             NET PAY: " + "%.2f".format (grossSalary/12 + annualBonusAmountOf/12 - (grossSalary/12*payePercentage/100+grossSalary/12*prsiPercentage/100 + cycleToWorkSchemeMonthlyDeduction))+ "                           |")
-    println ("|--------------------------------------------------------|")
+    println ("|                     NET PAY:${roundTwoDecimals(grossSalary/12 + annualBonusAmountOf/12 - (grossSalary/12*payePercentage/100+grossSalary/12*prsiPercentage/100 + cycleToWorkSchemeMonthlyDeduction))}                    |")
+    println ("----------------------------------------------------------")
 }
+
