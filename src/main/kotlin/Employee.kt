253e5f9bf5cabb8068ce8e58f9e7a5a751badbdb
Your Name <you@example.com>
2022-10-09 01:14:43 +0100

    Lab 03 -Create new class called EmployeeAPI -Add findAll, findOne, create functions to it -Cut FullName, MonthlySalary, MonthlyPRSI, MonthlyPAYE, GrossMonthlyPay, TotalMonthlyDeductions, NetMonthlyPay and Payslip functions from main.kt and paste them into Employee class -Remove redundant 'employee.' prefix in Employee class -Delete menu and main functions from main -Add new menu, start, list, search, internal, paySlip, dummyData, add and main functions to main -Update list method using Lambdas -Generate toString method to Employee class -Update list method again, removing toString function

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -10,4 +10,44 @@ class Employee (
     var prsiPercentage: Double,
     var annualBonusAmountOf: Double,
     var cycleToWorkSchemeMonthlyDeduction: Double
-)
+) {
+
+    fun getFullName() = when (gender) {
+        'm', 'M' -> "Mr. ${firstName} ${secondName}"
+        'f', 'F' -> "Ms.  ${firstName} ${secondName}"
+        else -> "${firstName} ${secondName}"
+    }
+
+    fun getMonthlySalary() = roundTwoDecimals(grossSalary / 12)
+    fun getMonthlyPRSI() = roundTwoDecimals(getMonthlySalary() * (prsiPercentage / 100))
+    fun getMonthlyPAYE() = roundTwoDecimals(getMonthlySalary() * (payePercentage / 100))
+    fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonusAmountOf / 12))
+    fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkSchemeMonthlyDeduction))
+
+    fun getNetMonthlyPay() = roundTwoDecimals(roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions()))
+
+
+
+    fun getPayslip() =
+        """            ----------------------------------------------------------
+            |                     Monthly Payslip                    |
+            |--------------------------------------------------------|
+            |                                                        |
+            |  Employee Name: ${getFullName()}       Employee ID: ${employeeID}   |
+            |                                                        |
+            |--------------------------------------------------------|
+            |  PAYMENT DETAILS           DEDUCTION DETAILS           |
+            |--------------------------------------------------------|
+            |  Salary: ${getMonthlySalary()}            PAYE: ${getMonthlyPAYE()}               |
+            |  Bonus:  ${roundTwoDecimals(annualBonusAmountOf / 12)}            PRSI: ${getMonthlyPRSI()}                |
+            |                            Cylce To Work: ${roundTwoDecimals(cycleToWorkSchemeMonthlyDeduction)}        |
+            |--------------------------------------------------------|
+            |  Gross: ${getGrossMonthlyPay()}            Total Deductions:${getTotalMonthlyDeductions()}    |
+            |--------------------------------------------------------|
+            |                     NET PAY:${getNetMonthlyPay()}                    |
+            ----------------------------------------------------------"""
+
+    override fun toString(): String {
+        return "Employee(firstName='$firstName', secondName='$secondName', gender=$gender, employeeID=$employeeID, grossSalary=$grossSalary, payePercentage=$payePercentage, prsiPercentage=$prsiPercentage, annualBonusAmountOf=$annualBonusAmountOf, cycleToWorkSchemeMonthlyDeduction=$cycleToWorkSchemeMonthlyDeduction)"
+    }
+}
